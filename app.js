c17afc2ff5ea7f1f35ae184f3c7073fe9ed285bd
Unknown <rgemert1@avans.nl>
2020-04-05 20:12:27 +0200

    A lot

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var passport = require('passport')
 var mongoose = require('mongoose');
-
+var cors = require('cors');
+var fileUpload = require('express-fileupload');
 require('./config/passport-config.js');
 
 var app = express();
@@ -16,7 +17,10 @@ var io = require('socket.io').listen(server);
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
+app.use(cors())
+app.use(fileUpload({
+  createParentPath: true
+}));
 // uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true, useUnifiedTopology:true});
@@ -26,6 +30,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+
 
 app.use('/', require('./routes/index.route'));
 app.use('/auth',require('./routes/auth.route'));
