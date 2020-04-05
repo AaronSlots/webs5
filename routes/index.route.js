0d2cea28b2a6e3c89a0035e9ed95a80b1273be87
Aaron Slots <aaronslots@gmail.com>
2020-04-05 10:20:46 +0200

    up

--- routes/index.route.js
+++ routes/index.route.js
@@ -4,27 +4,41 @@ var passport = require('passport')
 
 /* GET home page. */
 router.get('/', function(req, res){
-    let availableRoutes={
+    let data={
         availableRoutes: [
-            { method: 'GET', url: 'http://localhost:3000/' },
-            { method: 'GET', url: 'http://localhost:3000/profile' },
-            { method: 'GET', url: 'http://localhost:3000/auth/google' },
-            { method: 'GET', url: 'http://localhost:3000/auth/facebook' },
-            { method: 'GET', url: 'http://localhost:3000/auth/twitter' },
+            { method: 'GET', url: 'http://localhost:3000/', urlParams:[], queryParams:[], explanation:'show all routes of this api' },
+            { method: 'GET', url: 'http://localhost:3000/profile', urlParams:[], queryParams:[{name:'token',explanation:'YOUR-JWT',isRequired:true}], explanation:'see your profile' },
+            { method: 'GET', url: 'http://localhost:3000/auth/google', urlParams:[], queryParams:[], explanation:'login using google' },
+            { method: 'GET', url: 'http://localhost:3000/auth/facebook', urlParams:[], queryParams:[], explanation:'login using facebook' },
+            { method: 'GET', url: 'http://localhost:3000/auth/twitter', urlParams:[], queryParams:[], explanation:'login using twitter' },
         ]
     }
 
-    res.header('Content-Type',req.header('Content-Type'));
+    let header = 'text/html'
+    if(req.header('Content-Type') != null){
+        header = req.header('Content-Type')
+    }
+    res.header('Content-Type',header);
 
-    switch(req.header('Content-Type')){
-        case 'text/html': return res.render('index/index.ejs',availableRoutes);
-        default: return res.json(availableRoutes);
+    switch(header){
+        case 'text/html': return res.render('index/index.ejs',data);
+        default: return res.json(data);
     }
 });
 
 router.get('/profile',passport.authenticate('jwt',{session:false}), function(req, res){
     const token = req.query.token;
-	return res.render('index/profile.ejs',{user:req.user,token:token})
+    let data = {user:req.user,token:token}
+	let header = 'text/html'
+    if(req.header('Content-Type') != null){
+        header = req.header('Content-Type')
+    }
+    res.header('Content-Type',header);
+
+    switch(header){
+        case 'text/html': return res.render('index/profile.ejs',data);
+        default: return res.json(data);
+    }
 });
 
 module.exports = router;
